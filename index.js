c8119591e9db8ab04b6c6a15a4d2b12700413b9d
Johnny Wu <KarasAya@gmail.com>
2017-08-18 18:07:35 +0800

    add level

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 export { default as Component } from './lib/component';
 export { default as Entity } from './lib/entity';
+export { default as Level } from './lib/level';
 export { default as System } from './lib/system';
 export { default as Engine } from './lib/engine';
