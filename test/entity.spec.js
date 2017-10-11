4372f60e9e11b014abe0289de38e9383b23d8b55
shay_chen <641278529@qq.com>
2017-10-11 09:53:18 +0800

    Add entity test
    test 'enable' event:
      - emit in `engine.createEntity()`
      -emit when `entity.enabled`
      -emit after `entity.clone()` and `entity.deepClone()
      -emit when entity change parent and `enabledInHierarchy` change to true.
    
    test 'disable' event:
      -emit in `entity.destroy()`
      -emit in `entity.destroy()`
      -emit when entity change parent and `enabledInHierarchy` change to false.
    
    test 'ready' event
      -emit in `engine.createEntity()`
       -emit after `entity.clone()` and `entity.deepClone()

--- test/entity.spec.js
+++ test/entity.spec.js
@@ -1,9 +1,114 @@
 const tap = require('tap');
-const { Entity } = require('../dist/ecs');
+const { Engine, System, Component, Entity } = require('../dist/ecs');
 
 tap.test('entity', t => {
 
-  tap.test('constructor', t => {
+  tap.test('enable', t => {
+    let engine = new Engine();
+    let enableCount = 0;
+
+    //createEntity emit
+    let ent1 = engine.createEntity('Entity1');
+    ent1.on('enable', function () {
+      enableCount += 1;
+    });
+    engine.tick();
+    t.equal(enableCount, 1);
+
+    //entity.enabled true emit
+    ent1._onEnableChanged();
+    t.equal(enableCount, 2);
+
+    //cloneEntity emit
+    let ent2 = ent1.clone();
+    ent2.on('enable', function () {
+      enableCount += 1;
+    });
+    engine.tick();
+    t.equal(enableCount, 3);
+
+    //cloneEntity emit
+    let ent3 = ent1.deepClone();
+    ent3.on('enable', function () {
+      enableCount += 1;
+    });
+    engine.tick();
+    t.equal(enableCount, 4);
+
+    //entity parent change emit
+    let parentEnt = engine.createEntity('parentEnt');
+    engine.tick();
+    ent1._ancestorEnabled = false;
+    ent1.setParent(parentEnt);
+    t.equal(enableCount, 5);
+
+    t.end();
+  });
+
+  tap.test('disable', t => {
+
+    let engine = new Engine();
+    let disableCount = 0;
+
+    let ent1 = engine.createEntity('Entity1');
+    engine.tick();
+    ent1.on('disable', function () {
+      disableCount += 1;
+    });
+
+    //entity.enabled false emit
+    engine._notifyEnableChanged(ent1, false);
+    t.equal(disableCount, 1);
+
+    //destroy entity emit
+    ent1.destroy();
+    ent1.setParent(null);
+    engine.tick();
+    t.equal(disableCount, 2);
+
+    //entity parent change emit
+    let ent2 = engine.createEntity('Entity2');
+    ent2.on('disable', function () {
+      disableCount += 1;
+    });
+    let parentEnt = engine.createEntity('parentEnt');
+    engine.tick();
+    parentEnt._ancestorEnabled = false;
+    ent2.setParent(parentEnt);
+    t.equal(disableCount, 3);
+
+    t.end();
+  });
+
+  tap.test('ready', t => {
+
+    let engine = new Engine();
+    let readyCount = 0;
+
+    //createEntity emit
+    let ent1 = engine.createEntity('Entity1');
+    ent1.on('ready', function () {
+      readyCount += 1;
+    });
+    engine.tick();
+    t.equal(readyCount, 1);
+
+    //cloneEntity emit
+    let ent2 = ent1.clone();
+    ent2.on('ready', function () {
+      readyCount += 1;
+    });
+    engine.tick();
+    t.equal(readyCount, 2);
+
+    //deepCloneEntity emit
+    let ent3 = ent1.deepClone();
+    ent3.on('ready', function () {
+      readyCount += 1;
+    });
+    engine.tick();
+    t.equal(readyCount, 3);
+
     t.end();
   });
 
