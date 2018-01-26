4b9543dd236a364f95c44fe4b4ddd5e534de0905
Johnny Wu <karasaya@gmail.com>
2018-01-26 14:56:38 +0800

    fix inherit component not register events

--- test/component-events.spec.js
+++ test/component-events.spec.js
@@ -6,6 +6,37 @@ tap.test('component.events', t => {
   tap.test('register', t => {
     let app = new App();
     let onFooCount = 0;
+
+    class Foo extends Component {
+      constructor() {
+        super();
+      }
+
+      onFoo() {
+        onFooCount += 1;
+        t.assert(this instanceof Foo);
+      }
+    }
+    Foo.events = {
+      'foo': 'onFoo',
+    };
+
+    app.registerClass('Foo', Foo);
+
+    let ent1 = app.createEntity('Entity1');
+    ent1.addComp('Foo');
+    ent1.emit('foo');
+
+    app.tick();
+
+    t.equal(onFooCount, 1);
+
+    t.end();
+  });
+
+  tap.test('register with unused event', t => {
+    let app = new App();
+    let onFooCount = 0;
     let onBarCount = 0;
 
     class Foo extends Component {
@@ -33,6 +64,55 @@ tap.test('component.events', t => {
 
     t.equal(onFooCount, 1);
     t.equal(onBarCount, 0);
+
+    t.end();
+  });
+
+  tap.test('register with inherit', t => {
+    let app = new App();
+    let onFooCount = 0;
+    let onBarCount = 0;
+
+    class Foo extends Component {
+      constructor() {
+        super();
+      }
+
+      onFoo() {
+        onFooCount += 1;
+        t.assert(this instanceof Foo);
+      }
+    }
+    Foo.events = {
+      'foo': 'onFoo',
+    };
+
+    class Bar extends Foo {
+      constructor() {
+        super();
+      }
+
+      onBar() {
+        onBarCount += 1;
+        t.assert(this instanceof Foo);
+      }
+    }
+    Bar.events = {
+      'bar': 'onBar',
+    };
+
+    app.registerClass('Foo', Foo);
+    app.registerClass('Bar', Bar);
+
+    let ent1 = app.createEntity('Entity1');
+    ent1.addComp('Bar');
+    ent1.emit('foo');
+    ent1.emit('bar');
+
+    app.tick();
+
+    t.equal(onFooCount, 1);
+    t.equal(onBarCount, 1);
 
     t.end();
   });
@@ -91,6 +171,58 @@ tap.test('component.events', t => {
 
     t.equal(onFooCount, 2);
     t.equal(onBarCount, 2);
+
+    t.end();
+  });
+
+  tap.test('unregister with inherit', t => {
+    let app = new App();
+    let onFooCount = 0;
+    let onBarCount = 0;
+
+    class Foo extends Component {
+      constructor() {
+        super();
+      }
+
+      onFoo() {
+        onFooCount += 1;
+        t.assert(this instanceof Foo);
+      }
+    }
+    Foo.events = {
+      'foo': 'onFoo',
+    };
+
+    class Bar extends Foo {
+      constructor() {
+        super();
+      }
+
+      onBar() {
+        onBarCount += 1;
+        t.assert(this instanceof Foo);
+      }
+    }
+    Bar.events = {
+      'bar': 'onBar',
+    };
+
+    app.registerClass('Foo', Foo);
+    app.registerClass('Bar', Bar);
+
+    let ent1 = app.createEntity('Entity1');
+    let bar = ent1.addComp('Bar');
+    app.tick();
+
+    bar.destroy();
+    app.tick();
+
+    ent1.emit('foo');
+    ent1.emit('bar');
+
+    t.equal(onFooCount, 0);
+    t.equal(onBarCount, 0);
 
     t.end();
   });
